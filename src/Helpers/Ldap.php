3a9c5a185c1361e56d743a4e0cfbcb130ddc2fee
Eliel Ferreira <eliel_elie@hotmail.com>
2025-07-01 11:29:27 -0300

    Refactor: Improve LDAP helper class code style and logic

--- src/Helpers/Ldap.php
+++ src/Helpers/Ldap.php
@@ -2,13 +2,13 @@
 
 namespace LeafyTech\Core\Helpers;
 
-
 use Exception;
 use InvalidArgumentException;
 
 class Ldap
 {
     public const TYPE = 'LDAP';
+
     /**
      * The active LDAP connection.
      */
@@ -24,7 +24,7 @@ class Ldap
      *
      * @var bool
      */
-    protected $bound = false;
+    protected $bound  = false;
 
     /**
      * @var array|false
@@ -33,68 +33,68 @@ class Ldap
 
     public function __construct($configuration)
     {
-
         if (is_array($configuration)) {
             $this->configuration = $configuration;
+
             return $this;
         }
 
         throw new InvalidArgumentException(
             "Configuration must be array"
         );
-
     }
 
     public function connect()
     {
-        $hosts = explode(',',$this->configuration['host']);
+        $hosts = explode(',', $this->configuration['host']);
 
         foreach ($hosts as $host) {
-
             $this->connection = ldap_connect($host, $this->configuration['port']);
 
-            if($this->connection) {
+            if ($this->connection) {
                 foreach ($this->prepareOptions() as $option => $value) {
                     $this->setOption($option, $value);
                 }
+
                 return $this;
             }
         }
 
         throw new Exception('Failed to connect to LDAP hosts.');
-
     }
 
     public function close()
     {
         $connection = $this->connection;
-        return is_resource($connection) && ldap_close($connection);
 
+        return is_resource($connection) && ldap_close($connection);
     }
 
     public function memberOf($username = null, $devPrefix = null, $appPrefix = null)
     {
-        if(!$this->bound) $this->bindAsAdministrator();
-
-        if(is_null($username)) $username = $this->userId();
-
-        $resultSearch = $this->search($this->configuration['baseDn'], "sAMAccountName=".$username, ['memberof']);
-        $results      = $this->getEntries($resultSearch);
-        $memberOf     = [];
-
-        foreach($results[0]['memberof'] as $value){
-            $group = substr($value, 3, strpos($value, ",") - 3);
-            if(!is_null($devPrefix) && !is_null($appPrefix)) {
-                if(substr($group,0,9) == $devPrefix) {
-                    $permission = explode('_', $group);
-                    if(in_array($appPrefix, $permission)) {
-                        $memberOf[] = $group;
-                    }
-                }
-            } else {
-                $memberOf[] = $group;
-            }
+        if (!$this->bound) {
+            $this->bindAsAdministrator();
         }
+
+        if (is_null($username)) {
+            $username = $this->userId();
+        }
+
+        $resultSearch = $this->search($this->configuration['baseDn'], "sAMAccountName=" . $username, ['memberof']);
+        $results      = $this->getEntries($resultSearch);
+
+        $memberOf     = array_values(
+            array_filter(
+                array_map(
+                    fn ($value) => substr($value, 3, strpos($value, ",") - 3),
+                    $results[0]['memberof']
+                ),
+                fn ($group) => is_null($devPrefix) || is_null($appPrefix)
+                    ? true
+                    : strpos($group, $devPrefix) !== false && strpos($group, $appPrefix) !== false
+            )
+        );
+
         $this->freeResult($resultSearch);
 
         return $memberOf;
@@ -102,66 +102,68 @@ class Ldap
 
     public function userId()
     {
-        if(!is_null($this->info)) {
+        if (!is_null($this->info)) {
             return $this->info[0]['samaccountname'][0];
         }
-        return;
+
     }
 
     public function displayName()
     {
-        if(!is_null($this->info)) {
+        if (!is_null($this->info)) {
             return $this->info[0]['displayname'][0] ?? '';
         }
-        return;
+
     }
 
     public function getDepartmentName()
     {
-        if(!is_null($this->info)) {
+        if (!is_null($this->info)) {
             return $this->info[0]['department'][0] ?? '';
         }
-        return;
+
     }
 
     public function getEmail()
     {
-        if(!is_null($this->info)) {
+        if (!is_null($this->info)) {
             return $this->info[0]['mail'][0] ?? '';
         }
-        return;
+
     }
 
     public function getPersonalTitle()
     {
-        if(!is_null($this->info)) {
+        if (!is_null($this->info)) {
             return $this->info[0]['title'][0] ?? '';
         }
-        return;
+
     }
 
     public function getInfo($username = null)
     {
-        if (!is_null($username)){
-            if (!$this->bound) $this->bindAsAdministrator();
+        if (!is_null($username)) {
+            if (!$this->bound) {
+                $this->bindAsAdministrator();
+            }
             $resultSearch = $this->search($this->configuration['baseDn'], "sAMAccountName=" . $username, ['*']);
+
             return $this->getEntries($resultSearch);
         }
+
         return $this->info;
     }
 
     public function authenticate($username, $password)
     {
         try {
-
             $this->validateCredentials($username, $password);
 
-            if(@ldap_bind($this->connection, $this->applyPrefix($username), html_entity_decode($password))) {
+            if (@ldap_bind($this->connection, $this->applyPrefix($username), html_entity_decode($password))) {
                 $this->bound  = true;
-                $resultSearch = $this->search($this->configuration['baseDn'], "sAMAccountName=".$username, ['*']);
+                $resultSearch = $this->search($this->configuration['baseDn'], "sAMAccountName=" . $username, ['*']);
                 $this->info   = $this->getEntries($resultSearch);
             }
-
         } catch (\Exception $e) {
             $this->bound = false;
         }
@@ -221,7 +223,8 @@ class Ldap
     protected function applyPrefix($username)
     {
         $prefix = $this->configuration['accountPrefix'];
-        return $prefix.'\\'.$username;
+
+        return $prefix . '\\' . $username;
     }
 
     public function bindAsAdministrator()
